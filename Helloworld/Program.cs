96c1dd7f1296e1d9ebc6f32e65fae32a1a118834
morganjones4 <morganjones4@mail.usf.edu>
2019-09-02 14:35:43 -0400

    Version 2- Hello World

--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -6,7 +6,10 @@ namespace Helloworld
     {
         static void Main(string[] args)
         {
+            //Pause to diplay Hello World!
+            Console.WriteLine("Press any key to continue...");
             Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
         }
     }
 }
